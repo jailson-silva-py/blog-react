b122126037a9eee7364fc79ea236ad00d312db1f
CruelSun0 <jailsonsunshime@gmail.com>
2025-06-23 18:01:33 -0300

    Adicionando tipagem aos .png e .module.css para contornar erro do Typescript

---
+++ src/types/global.d.ts
@@ -0,0 +1,15 @@
+declare module '*.module.css' {
+
+    const classes: {[key:string]:string}
+
+    export default classes
+
+}
+
+declare module '*.png' {
+
+    const image: string
+
+    export default image
+
+}
