0c55da4a3eef790c245a69cbae6ded269f9bef96
CruelSun0 <jailsonsunshime@gmail.com>
2025-06-23 18:15:10 -0300

    Removendo importações desnecessárias

---
+++ src/pages/Contato.tsx
@@ -0,0 +1,77 @@
+import classes from '../styles/Contato.module.css'
+import { useState, useCallback } from 'react'
+import CampoAreaInput from '../components/CampoAreaInput/CampoAreaInput'
+
+const email = "j4ilsonsilv4pereir4@gmail.com"
+
+
+const Contact = () => {
+
+    const [copiado, setCopiado] = useState(false)
+    const copiar =  useCallback(() => {
+
+    navigator.clipboard.writeText(email)
+    .then(() => {
+
+        setCopiado(true)
+        setTimeout(() => setCopiado(false), 2000)
+
+    })
+
+    .catch((err) => {
+
+        console.error("Erro ao copiar: ", err)
+
+    })
+    }, [])
+    
+
+    return (
+
+        <>
+        
+            <main id="content">
+
+                {copiado && <span className={classes.toast}>E-mail copiado com sucesso!</span>}
+
+                <section className={classes.intro}>
+                <h1 className={classes.titulo}>Entre em contato</h1>
+
+                <label className={classes.emailContent}>
+                <span onClick={copiar} className={classes.email}>{email}</span>
+                <a href="mailto:j4ilsonsilv4pereir4@gmail.com">Enviar E-mail</a>
+                </label>
+                </section>
+
+                <section className={classes.entreEmContato}>
+
+                <h1>Mande uma mensagem:</h1>
+
+                <form className={classes.formulario}>
+
+                <label className={classes.campoForm}>
+                <span className={classes.campoTitulo}>Nome:</span>
+                <input type="text" placeholder="Seu nome" className={classes.padraoInput} required />
+                </label>
+
+                <label className={classes.campoForm}>
+                <span className={classes.campoTitulo}>E-mail:</span>
+                <input type="email" placeholder="Seu e-mail" className={classes.padraoInput} required />
+                </label>
+
+                <CampoAreaInput texto="Comentario: " dica="Digite seu comentário ..."/>
+                <button type="submit" className={classes.padraoSubmit}>Enviar</button>
+                </form>
+                </section>
+
+
+            </main>
+
+        </>
+
+    )
+
+}
+
+
+export default Contact
