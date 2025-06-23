bccfc396b985f0ccec8630a9e3f04b23750845f7
CruelSun0 <jailsonsunshime@gmail.com>
2025-06-23 17:34:49 -0300

    Adicionando configurações para testes (vitest + react test library)

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+
+    globals:true,
+    environment:"jsdom",
+    setupFiles:'src/setupTest.ts',
+    coverage: {
+
+      exclude:['src/types', 'src/main.tsx']
+
+    }
+
+  }
+})
